fb25398e58a135723151a4436b1ffe17e0b426e4
ssandovalk10 <ssandovalk10@gmail.com>
2023-08-19 19:01:22 +0000

    Modificacion de producto

--- src/main/java/co/edu/usbcali/tiendaApp/controller/ProductoController.java
+++ src/main/java/co/edu/usbcali/tiendaApp/controller/ProductoController.java
@@ -4,15 +4,7 @@ import co.edu.usbcali.tiendaApp.domain.Producto;
 import co.edu.usbcali.tiendaApp.dto.ProductoDTO;
 import co.edu.usbcali.tiendaApp.request.CrearProductoRequest;
 import co.edu.usbcali.tiendaApp.response.CrearProductoResponse;
-import co.edu.usbcali.tiendaApp.service.ProductoService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
 @CrossOrigin
 @RequestMapping("/producto")
 public class ProductoController {
