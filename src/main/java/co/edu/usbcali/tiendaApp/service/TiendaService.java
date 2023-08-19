d29b6e0ca044e785662179a0284f4e60905a9bad
ssandovalk10 <ssandovalk10@gmail.com>
2023-08-19 19:11:21 +0000

    Modificacion desde le commit - se agregan 2 files

--- src/main/java/co/edu/usbcali/tiendaApp/service/TiendaService.java
+++ src/main/java/co/edu/usbcali/tiendaApp/service/TiendaService.java
@@ -1,23 +1,19 @@
 package co.edu.usbcali.tiendaApp.service;
 
-import co.edu.usbcali.tiendaApp.request.CrearCategoriaRequest;
+
 import co.edu.usbcali.tiendaApp.response.CrearCategoriaResponse;
 
 import java.util.List;
 
-public interface CategoriaService {
+public interface TiendaService {
     List<CategoriaDTO> obtenerTodos();
 
-    CategoriaDTO buscarPorId(Integer id) throws Exception;
-
-    Categoria buscarCategoriaPorId(Integer id) throws Exception;
-
-    CategoriaDTO guardar(CategoriaDTO categoriaDTO) throws Exception;
-
-    CategoriaDTO actualizar(CategoriaDTO categoriaDTO) throws Exception;
     public String eliminarById(Integer id) throws Exception;
 
     List<CategoriaDTO> buscarPorNombreLike(String nombre) throws Exception;
 
     CrearCategoriaResponse crearCategoria(CrearCategoriaRequest crearCategoriaRequest) throws Exception;
 }
+
+
+
